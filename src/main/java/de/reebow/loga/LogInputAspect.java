43f0a672f7afb7989b5d2107eb69c5616c5b482b
Tim Rutana <tim.rutana@sap.com>
2020-02-02 20:33:12 +0100

    fixes #1 improve performance in case log level is not enabled

--- src/main/java/de/reebow/loga/LogInputAspect.java
+++ src/main/java/de/reebow/loga/LogInputAspect.java
@@ -4,7 +4,6 @@ import de.reebow.loga.annotations.LogInput;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.spi.StandardLevel;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -19,11 +18,26 @@ public class LogInputAspect {
     Logger logger = LogManager.getLogger(signature.getDeclaringType());
 
     LogInput annotation = signature.getMethod().getAnnotation(LogInput.class);
-    StandardLevel level = annotation.value();
+    Level level = Level.valueOf(annotation.value().name());
+
+    if (!logger.isEnabled(level)) {
+      return;
+    }
+
     Class[] parameterTypes = signature.getParameterTypes();
     String[] parameterNames = signature.getParameterNames();
     Object[] args = joinPoint.getArgs();
+    String parameterTypesAndValuesStrings = createParameterAndValueString(parameterTypes, parameterNames, args);
+
+    String logString = String
+      .format("Input arguments for method \"%s\": %s", signature.getName(), parameterTypesAndValuesStrings);
+
+    logger.log(level, logString);
+  }
+
+  private String createParameterAndValueString(Class[] parameterTypes, String[] parameterNames, Object[] args) {
     StringBuilder parameterTypesAndValuesStrings = new StringBuilder();
+
     for (int i = 0; i < parameterTypes.length; i++) {
       Object arg = args[i];
       String argument;
@@ -35,9 +49,6 @@ public class LogInputAspect {
       parameterTypesAndValuesStrings.append("Parameter type: ").append(parameterTypes[i]).append("Parameter name: ")
         .append(parameterNames[i]).append(" value: ").append(argument).append(". ");
     }
-
-    String logString = String
-      .format("Input arguments for method \"%s\": %s", signature.getName(), parameterTypesAndValuesStrings);
-    logger.log(Level.valueOf(level.name()), logString);
+    return parameterTypesAndValuesStrings.toString();
   }
 }
